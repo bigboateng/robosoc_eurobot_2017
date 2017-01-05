646bbad9ba96e0a8c1f66a8eba87d1e39474cde6
bigboateng <bigboateng2011@gmail.com>
2017-01-05 15:58:41 +0000

    update

--- src/Main/nav_node.py
+++ src/Main/nav_node.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+# Messages
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Quaternion
+
+
 from MD25 import MD25 
 controller = MD25(0x58,1,True)
 
+def getPose(left_count, right_count):
+	# returns pose infomation
+	
 def update_ukf():
 	# Function to send pose data to ukf node
 
 	# init node
 	rospy.init_node("nav_node", anonymous=True)
 	# update every 1 second
-	rate = rospy.Rate(10)
+	rate = rospy.Rate(1)
 	# odom publisher
 	odom_publisher = rospy.Publisher("odometry_publisher", String, queue_size=10)
 	count = 0
 	while not rospy.is_shutdown():
-		leftEncoder, rightEncoder = controller.getEncoderValues()
-		rospy.loginfo("Left={},right={}".format(leftEncoder,rightEncoder))
+		#leftEncoder, rightEncoder = controller.getEncoderValues()
+		#rospy.loginfo("Left={},right={}".format(leftEncoder,rightEncoder))
+		rospy.loginfo("Hello");
 		rate.sleep()
 		
 
