73f5c76adaf62245ec06bfc8ad19a264867e8a90
Kanter666 <ak2g15@soton.ac.uk>
2017-03-25 16:15:56 +0000

    First code for button interrupt

---
+++ src/PrimaryRobot/StartButton.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+import RPi.GPIO as GPIO  
+
+# GPIO 23 set up as input. It is pulled up to stop false signals  
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    GPIO.add_event_detect(11, GPIO.FALLING, callback=my_callback, bouncetime=300)  
+    while not rospy.is_shutdown():
+        try:  
+            GPIO.wait_for_edge(23, GPIO.FALLING)  
+            print "\nFalling edge detected. Now your program can continue with"  
+            print "whatever was waiting for a button press."
+            hello_str = "Button pressed %s" % rospy.get_time()  
+        except KeyboardInterrupt:  
+            GPIO.cleanup()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
