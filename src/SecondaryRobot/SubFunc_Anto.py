3b613be2a77c12862425fd68c122a2efba268fa9
bigboateng <bigboateng2011@gmail.com>
2017-04-02 13:03:49 +0100

    Working on ultrasonic sensors

--- src/SecondaryRobot/SubFunc_Anto.py
+++ src/SecondaryRobot/SubFunc_Anto.py
@@ -5,8 +5,6 @@ sys.path.append('../UltrasonicSensors')
 from SRF08 import SRF08
 sys.path.append('../IRSensors')
 from IRsensor import IRsensor
-sys.path.append('../ESC')
-from ESC import ESC
 import time
 
 # Parameters
@@ -21,13 +19,6 @@ channel_mcp_IR_sensor_left = 1
 address_i2c_US_sensor_right = 0xE0 >> 1
 address_i2c_US_sensor_left = 0xE2 >> 1 # CHANGE ADDRESS
 
-# PCA9685
-address_i2c_PCA9685 = 0x40
-# ESC
-channel_PCA9685_ESC = 15
-esc.setPulseLengthMin(1000)
-esc.setPulseLengthMax(2000)
-
 
 # Initialisation 
 
@@ -40,9 +31,6 @@ IR_sensor_left = IRsensor(SPI_PORT, SPI_DEVICE, channel_mcp_IR_sensor_left)
 US_sensor_right = SRF08(address_US_sensor_right, 2)
 # Ultrasonic Sensor LEFT
 US_sensor_left = SRF08(address_US_sensor_left, 2) 
-
-# ESC
-esc = ESC(address_i2c_ESC, channel_PCA9685_ESC, 50)
 
 
 # Functions
@@ -59,14 +47,10 @@ def getDistance_USsensor():
     US_sensor_right.echo[0] # output this to ROS
     US_sensor_left.echo[0] # output this to ROS
 
-def setSpeedESC(speed):
-    esc.setSpeed(speed)
+def init_node():
+    rospy.init_node("")
 
-def setBallGrabberOn(): # Rename ?
-    esc.setSpeed(23)
 
-def setBallGrabberOff():
-    esc.setSpeed(0)
 
 
 
